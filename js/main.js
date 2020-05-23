980b2a4ca1e3a41ee8dc145f2e5cf5f5546f928d
nabeel-khanjee <59483644+nabeel-khanjee@users.noreply.github.com>
2020-05-23 15:34:41 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+$(document).ready(function() {  
+  $(".slider").slider({ full_width: true });  
+});  
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+$(document).ready(function(){
+  $('.fixed-action-btn').floatingActionButton();
+});
